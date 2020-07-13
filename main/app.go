881e13abe21c19d12d751afb54aa8cece4954520
SarahAyuNanda <nanda.sarahayu@gmail.com>
2020-07-13 12:16:26 +0700

    Done making app.go

---
+++ main/app.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/golang/sarahayunanda/goday6/complete_number/utils"
+
+func main() {
+	// target => 4283 * 188 = 805204
+	expression := "42#3 * 188 = 80#204"
+	utils.Solving(expression)
+}
